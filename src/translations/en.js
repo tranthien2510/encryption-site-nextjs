83857aba6b1f4cb8398ea20e3b241391ba07dbb8
Minh Chu <scsilver94@gmail.com>
2021-09-03 00:51:48 +0700

    Add page title and clean css

--- src/translations/en.js
+++ src/translations/en.js
@@ -2,5 +2,6 @@ export default {
     encrypt: 'Encrypt',
     decrypt: 'Decrypt',
     chooseAFile: 'Choose a file',
-    enterPassword: 'Enter password'
+    enterPassword: 'Enter password',
+    pageTitle: 'Encrypt - Decrypt'
 }
--- src/translations/vi.js
+++ src/translations/vi.js
@@ -2,6 +2,7 @@ export default {
     encrypt: 'Mã hóa',
     decrypt: 'Giải mã',
     chooseAFile: 'Chọn tệp',
-    enterPassword: 'Nhập mật khẩu'
+    enterPassword: 'Nhập mật khẩu',
+    pageTitle: 'Mã hóa - Giải mã'
 }
 
